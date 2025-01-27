42bac4056c8eb71acc6bda402a2cdaabbe19bfab
JanSteinhauer <insta.steini4@gmail.com>
2025-01-27 15:47:02 -0600

    inital commit

---
+++ MindSphere/MindSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindSphereApp.swift
+//  MindSphere
+//
+//  Created by Steinhauer, Jan on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MindSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
